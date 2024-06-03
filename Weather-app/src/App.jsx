f2086db1dfea105c374b647bf80c5209c6837edb
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-06-03 13:25:33 +0530

    update in App.jsx file

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Welcome Weather APP</h1>
+      <p className="text-xl text-red-500">
+        Here You can explore accurate data on weather so you can enjoy your day.
+      </p>
     </div>
   );
 }
